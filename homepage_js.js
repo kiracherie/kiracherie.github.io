b7392b98dfcab766f8df31fc545c0d08aa9fdada
Kira <56455045+kiracherie@users.noreply.github.com>
2020-08-10 07:31:42 -0700

    add resume and style changes

---
+++ homepage_js.js
@@ -0,0 +1,23 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+function addResponsive() {
+    console.log("adding responsive class\n")
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
